9f2fb5a55f5440477f24b3f9d1ca974c2f4c3751
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-17 22:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,27 @@ Created on Thu Jun 11 22:15:24 2020
 
 
 
+import numpy
+from keras.models import load_model
+import cv2
+from cv2 import imread
+import numpy as np
+from tensorflow.keras.models import load_model
+import cv2
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
 from flask import request, jsonify, Flask, render_template
+import base64
+import io
+from PIL import Image
+from keras.initializers import glorot_uniform
 
 
 app = Flask(__name__)
        
 @app.route('/')
 def home():
-	return render_template("home.html")
+	return render_template('home.html')
 
 
 
