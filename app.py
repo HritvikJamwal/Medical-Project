84774fd3816e73a641e0e2dab3b6b207db179ab3
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-20 23:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,36 +6,28 @@ Created on Thu Jun 11 22:15:24 2020
 """
 
 
-
 import numpy
+from keras.models import load_model
+import cv2
+from cv2 import imread
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+
 from flask import request, jsonify, Flask, render_template
-import base64
-import io
+
 from PIL import Image
+from keras.initializers import glorot_uniform
 
 
-
-app = Flask(__name__)
-       
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.run()
+model = load_model('malaria_model.h5')
     
+def malaria():
+    data = image.load_img('infectedmalaria.jpeg', target_size=(100, 100, 3))
+    data = np.expand_dims(data, axis=0)
+    data = data * 1.0 / 255
+    predicted = model.predict(data)
+    print("Chances of infected cell:", (("%0.2f"%(1-predicted))))
     
-    
-    
+          
+malaria()
