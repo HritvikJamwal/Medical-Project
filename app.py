e900c66341144f7e8e5cdd22046cb343c2d80366
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-21 00:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ Created on Thu Jun 11 22:15:24 2020
 @author: hritvik
 """
 
-
+from keras.models import load_model
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 from flask import request, jsonify, Flask, render_template
-
+from keras.initializers import glorot_uniform
 
 app = Flask(__name__)
 
@@ -20,13 +20,12 @@ app = Flask(__name__)
    
 @app.route("/")
 def malaria():
-    
     model = load_model('malaria_model.h5')
     data = image.load_img('normalmalaria.jpeg', target_size=(100, 100, 3))
     data = np.expand_dims(data, axis=0)
     data = data * 1.0 / 255
     predicted = model.predict(data)
-    return render_template('home.html', prediction = (("%0.2f"%(1-predicted))))
+    return render_template('home.html')
     
           
 
