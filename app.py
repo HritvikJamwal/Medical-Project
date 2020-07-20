ca80b4233fe429fbed1f0032746c0d89d95b5d07
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-21 00:58:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ Created on Thu Jun 11 22:15:24 2020
 @author: hritvik
 """
 
-from keras.models import load_model
+
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 from flask import request, jsonify, Flask, render_template
-from keras.initializers import glorot_uniform
+
 
 app = Flask(__name__)
 
