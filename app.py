4b00ed13f806351551dd6beb2c0ab56270ab2b0d
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-21 00:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ app = Flask(__name__)
 @app.route("/")
 def malaria():
     
-    return render_template('home.html', prediction = 'hello')
+    model = load_model('malaria_model.h5')
+    data = image.load_img('normalmalaria.jpeg', target_size=(100, 100, 3))
+    data = np.expand_dims(data, axis=0)
+    data = data * 1.0 / 255
+    predicted = model.predict(data)
+    return render_template('home.html', prediction = (("%0.2f"%(1-predicted))))
     
           
 
