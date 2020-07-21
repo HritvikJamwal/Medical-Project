dd484a84a8c02d89e2d594934a917548146189bb
HritvikJamwal <42890458+HritvikJamwal@users.noreply.github.com>
2020-07-21 19:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ app = Flask(__name__)
    
 @app.route("/")
 def malaria():
-    model = load_model('new_malaria_model.h5')
-    data = image.load_img('infectedmalaria.jpeg', target_size=(50, 50, 3))
+    model = load_model('malaria_model.h5')
+    data = image.load_img('infectedmalaria.jpeg', target_size=(100, 100, 3))
     data = np.expand_dims(data, axis=0)
     data = data * 1.0 / 255
     predicted = model.predict(data)
